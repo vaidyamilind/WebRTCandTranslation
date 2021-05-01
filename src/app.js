bc02984fa16b8f44ff2e3de55acfe29b61b616c1
Milind <milind@pop-os.localdomain>
2021-05-01 22:55:18 +0530

    record and save wav file locally

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
 import httpd from "http";
-import {Server as socketIO} from "socket.io"
-import path from "path"
+import { Server as socketIO } from "socket.io";
+import path from "path";
+import fs from "fs";
 
 const app = express();
-const http = httpd.createServer(app)
+const http = httpd.createServer(app);
 const io = new socketIO(http);
 const __dirname = path.resolve();
 
@@ -19,13 +20,18 @@ app.get("/", (req, res) => {
 io.on("connect", (client) => {
     console.log(`Client connected [id=${client.id}]`);
     client.emit("server_setup", `Server connected [id=${client.id}]`);
-})
+
+    client.on("message", async function (data) {
+        const dataURL = data.audio.dataURL.split(",").pop();
+        let fileBuffer = Buffer.from(dataURL, "base64");
+        fs.writeFile("hello.wav", fileBuffer, (err) => {
+            if (err) throw err;
+            console.log("Saved!");
+        });
+        client.emit("results", "HELLO");
+    });
+});
 
 http.listen(3000, () => {
     console.log("listening on *:3000");
 });
-  
-
-
-
-
